e3dcd5af4c4bd510fbffd9271562e763529d5675
wangzhuolan <1437286101@qq.com>
2022-05-09 23:01:05 +0800

    提交

---
+++ cs131_numpy_entry_level/imageManip.py
@@ -0,0 +1,224 @@
+import math
+
+import numpy as np
+from PIL import Image
+from skimage import color, io
+from skimage.color import rgb2gray
+
+
+def load(image_path):
+    """Loads an image from a file path.
+
+    HINT: Look up `skimage.io.imread()` function.
+
+    Args:
+        image_path: file path to the image.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    # Use skimage io.imread
+    ##out = skimage.io.imread(image_path) 错误写法，因为没有引入skimage
+    out = io.imread(image_path)
+    ### END YOUR CODE
+
+    # Let's convert the image to be between the correct range.
+    out = out.astype(np.float64) / 255
+    return out
+
+
+def dim_image(image):
+    """Change the value of every pixel by following
+
+                        x_n = 0.5*x_p^2
+
+    where x_n is the new value and x_p is the original value.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    out = image**2*0.5
+    ### END YOUR CODE
+
+    return out
+
+
+def convert_to_grey_scale(image):
+    """Change image to gray scale.
+
+    HINT: Look at `skimage.color` library to see if there is a function
+    there you can use.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    out = rgb2gray(image)
+    ### END YOUR CODE
+
+    return out
+
+
+def rgb_exclusion(image, channel):
+    """Return image **excluding** the rgb channel specified
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "R", "G" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+
+    ### YOUR CODE HERE
+    out = image.copy()
+    ##需要deepcopy
+    if channel == "R":
+        out[:,:,0] = 0
+    elif channel == "G":
+        out[:,:,1] = 0
+    else:
+        out[:,:,2] = 0
+
+    ### END YOUR CODE
+
+    return out
+
+
+def lab_decomposition(image, channel):
+    """Decomposes the image into LAB and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "L", "A" or "B".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    lab = color.rgb2lab(image)
+    out = None
+
+    ### YOUR CODE HERE
+    if channel == "L":
+        out = lab[:,:,0]
+    elif channel == "A":
+        out = lab[:,:,1]
+    else:
+        out = lab[:,:,2]
+    ### END YOUR CODE
+
+    return out
+
+
+def hsv_decomposition(image, channel='H'):
+    """Decomposes the image into HSV and only returns the channel specified.
+
+    Args:
+        image: numpy array of shape(image_height, image_width, 3).
+        channel: str specifying the channel. Can be either "H", "S" or "V".
+
+    Returns:
+        out: numpy array of shape(image_height, image_width).
+    """
+
+    hsv = color.rgb2hsv(image)
+    out = None
+
+    ### YOUR CODE HERE
+    if channel == "H":
+        out = hsv[:,:,0]
+    elif channel == "S":
+        out = hsv[:,:,1]
+    else:
+        out = hsv[:,:,2]
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_images(image1, image2, channel1, channel2):
+    """Combines image1 and image2 by taking the left half of image1
+    and the right half of image2. The final combination also excludes
+    channel1 from image1 and channel2 from image2 for each image.
+
+    HINTS: Use `rgb_exclusion()` you implemented earlier as a helper
+    function. Also look up `np.concatenate()` to help you combine images.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+        image2: numpy array of shape(image_height, image_width, 3).
+        channel1: str specifying channel used for image1.
+        channel2: str specifying channel used for image2.
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+
+    out = None
+    ### YOUR CODE HERE
+    image1_ex = rgb_exclusion(image1,channel1)
+    image2_ex = rgb_exclusion(image2,channel2)
+    left = image1_ex[:, :np.shape(image1_ex)[1]//2, :]
+    right = image2_ex[:, np.shape(image2_ex)[1]//2: ,:]
+    #out = np.hstack((left,right))
+    out = np.concatenate([left,right],axis=1)
+    ### END YOUR CODE
+
+    return out
+
+
+def mix_quadrants(image):
+    """THIS IS AN EXTRA CREDIT FUNCTION.
+
+    This function takes an image, and performs a different operation
+    to each of the 4 quadrants of the image. Then it combines the 4
+    quadrants back together.
+
+    Here are the 4 operations you should perform on the 4 quadrants:
+        Top left quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+        Top right quadrant: Dim the quadrant using `dim_image()`.
+        Bottom left quadrant: Brighthen the quadrant using the function:
+            x_n = x_p^0.5
+        Bottom right quadrant: Remove the 'R' channel using `rgb_exclusion()`.
+
+    Args:
+        image1: numpy array of shape(image_height, image_width, 3).
+
+    Returns:
+        out: numpy array of shape(image_height, image_width, 3).
+    """
+    out = None
+
+    ### YOUR CODE HERE
+    image_ex1 = rgb_exclusion(image,"R")
+    image_ex2 = dim_image(image)
+    image_ex3 = image**0.5
+    image_ex4 = rgb_exclusion(image,"R")
+    Top_left = image_ex1[:np.shape(image_ex1)[0]//2, :np.shape(image_ex1)[1]//2, :]
+    Top_right = image_ex2[:np.shape(image_ex2)[0]//2, np.shape(image_ex2)[1]//2:, :]
+    Bottom_left = image_ex3[np.shape(image_ex3)[0]//2:, :np.shape(image_ex3)[1]//2, :]
+    Bottom_right = image_ex4[np.shape(image_ex4)[0]//2:, np.shape(image_ex4)[1]//2:, :]
+    out1 = np.concatenate([Top_left,Top_right],axis=1)
+    out2 = np.concatenate([Bottom_left,Bottom_right],axis=1)
+    out = np.concatenate([out1,out2],axis=0)
+    ### END YOUR CODE
+
+    return out
